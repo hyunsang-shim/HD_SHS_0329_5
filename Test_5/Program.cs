b6198b19682976c3ba77e163ecab94a6259c5e6c
hyunsang-shim <shimhyunsang.dev@gmail.com>
2022-03-29 22:05:22 +0900

    혼자해보기 5번
    devkoboso/403
    혼자해보기 5번 풀이

--- Test_5/Program.cs
+++ Test_5/Program.cs
@@ -4,9 +4,58 @@ namespace Test_5
 {
     class Program
     {
+        // 5. for와 while을 혼합하여 아래 별을 출력하세요
+        // ㅁㅁㅁ별
+        // ㅁㅁ별별
+        // ㅁ별별별
+        // 별별별별
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //solution 1
+            Console.WriteLine("solution 1");
+            int lines = 4;
+            while (lines > 0)
+            {
+                for (int i = 0; i < lines - 1; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 4 - lines + 1; j > 0; j--)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+
+                lines--;
+            }
+
+            // 구분선
+            Console.WriteLine("------------------");
+
+            // solution 2
+            Console.WriteLine("solution 2");
+            for (int i = 0; i < 4; i++)
+            {
+                int spaces = 3 - i;
+                int stars2 = 1 + i;
+                
+                while (spaces > 0)
+                {
+                    Console.Write(" ");
+                    spaces--;
+                }
+
+                while (stars2 > 0)
+                {
+                    Console.Write("*");
+                    stars2--;
+                }
+
+                Console.Write("\n");
+            }
         }
     }
 }
